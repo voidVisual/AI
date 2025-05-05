b55a4685ac6386633c7f4a6c87ad959014c7a752
VoidVisual <154327945+voidVisual@users.noreply.github.com>
2025-05-05 12:35:38 +0530

    Update kruskal.py

--- kruskal.py
+++ kruskal.py
@@ -1,19 +1,19 @@
 class Graph:
     def __init__(self, vertices):
-        self.V = vertices  # Number of vertices
-        self.graph = []    # Graph will be stored as a list of edges
+        self.V = vertices  
+        self.graph = []    
 
-    # Function to add an edge to the graph
+    
     def addEdge(self, u, v, w):
         self.graph.append([u, v, w])
 
-    # A utility function to find set of an element i (uses path compression)
+   
     def find(self, parent, i):
         if parent[i] != i:
             parent[i] = self.find(parent, parent[i])
         return parent[i]
 
-    # A function that does union of two sets x and y (uses union by rank)
+    
     def union(self, parent, rank, x, y):
         if rank[x] < rank[y]:
             parent[x] = y
@@ -23,38 +23,37 @@ class Graph:
             parent[y] = x
             rank[x] += 1
 
-    # The main function to construct MST using Kruskal's algorithm
+    
     def KruskalMST(self):
-        result = []  # Store the resultant MST
-        i = 0        # Index for sorted edges
-        e = 0        # Index for result[]
+        result = []  
+        i = 0        
+        e = 0        
 
-        # Step 1: Sort all the edges in non-decreasing order of their weight
         self.graph = sorted(self.graph, key=lambda item: item[2])
 
         parent = []
         rank = []
 
-        # Create V subsets with single elements
+        
         for node in range(self.V):
             parent.append(node)
             rank.append(0)
 
-        # Number of edges to be taken is V-1
+        
         while e < self.V - 1:
-            # Step 2: Pick the smallest edge and increment the index
+            
             u, v, w = self.graph[i]
             i += 1
             x = self.find(parent, u)
             y = self.find(parent, v)
 
-            # If including this edge doesn't cause cycle, include it in result
+            
             if x != y:
                 e += 1
                 result.append([u, v, w])
                 self.union(parent, rank, x, y)
 
-        # Print the constructed MST
+       
         minimumCost = 0
         print("Edges in the constructed MST:")
         for u, v, weight in result:
@@ -62,7 +61,7 @@ class Graph:
             print(f"{u} -- {v} == {weight}")
         print("Minimum Spanning Tree cost:", minimumCost)
 
-# Driver code
+
 if __name__ == '__main__':
     g = Graph(4)
     g.addEdge(0, 1, 10)
